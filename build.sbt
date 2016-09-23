1a7ba57c3f7c13e48b14e3064eb645067893b69c
kaja47 <kaja47@k47.cz>
2016-09-23 23:08:55 +0200

    diff/patch for sequences

---
+++ build.sbt
@@ -0,0 +1,9 @@
+organization := "atrox"
+
+name := "diff"
+
+version := "0.1-SNAPSHOT"
+
+scalaVersion := "2.11.8"
+
+libraryDependencies ++= Seq()
---
+++ diff.scala
@@ -0,0 +1,145 @@
+package atrox
+
+import collection.mutable.ArrayBuilder
+import collection.mutable.ArrayBuffer
+
+object diff {
+
+  sealed trait Diff[T] { def pos: Int }
+  case class Add[T](pos: Int, values: Seq[T]) extends Diff[T]
+  case class Del[T](pos: Int, length: Int) extends Diff[T]
+
+
+  // Longest common subsequence
+  def lcs[T](a: Seq[T], b: Seq[T]): Seq[T] = {
+    val alen = a.length
+    val blen = b.length
+
+    val lengths: Array[Array[Int]] = Array.fill(alen+1) { new Array[Int](blen+1) }
+
+    // row 0 and column 0 are initialized to 0 already
+
+    var i = 0; while (i < alen) {
+      var j = 0; while (j < blen) {
+        if (a(i) == b(j)) {
+          lengths(i+1)(j+1) = lengths(i)(j) + 1
+        } else {
+          lengths(i+1)(j+1) = math.max(lengths(i+1)(j), lengths(i)(j+1))
+        }
+
+        j += 1
+      }
+      i += 1
+    }
+
+    // read the substring out from the matrix
+    var res = List[T]()
+    var x = alen
+    var y = blen
+    while (x != 0 && y != 0) {
+      if (lengths(x)(y) == lengths(x-1)(y))
+        x -= 1
+      else if (lengths(x)(y) == lengths(x)(y-1))
+        y -= 1
+      else {
+        assert(a(x-1) == b(y-1))
+        res = a(x-1) :: res
+        x -= 1
+        y -= 1
+      }
+    }
+
+    res
+  }
+
+
+
+  def diff[T](a: Seq[T], b: Seq[T]): Seq[Diff[T]] = {
+
+    val diffs = new ArrayBuilder.ofRef[Diff[T]]()
+    val cs = lcs(a, b)
+    var apos, bpos = 0
+
+    for (c <- cs) {
+      val aDiffPos = apos
+
+      var dels = 0
+      while (apos < a.length && a(apos) != c) {
+        dels += 1
+        apos += 1
+      }
+      if (dels > 0) {
+        diffs += Del(aDiffPos, dels)
+      }
+      apos += 1
+
+      val adds = ArrayBuffer[T]()
+      while (bpos < b.length && b(bpos) != c) {
+        adds += b(bpos)
+        bpos += 1
+      }
+      if (adds.length > 0) {
+        diffs += Add(aDiffPos, adds)
+      }
+      bpos += 1
+    }
+
+    val aDiffPos = apos
+
+    var dels = 0
+    while (apos < a.length) {
+      dels += 1
+      apos += 1
+    }
+    if (dels > 0) {
+      diffs += Del(aDiffPos, dels)
+    }
+
+    val adds = ArrayBuffer[T]()
+    while (bpos < b.length) {
+      adds += b(bpos)
+      bpos += 1
+    }
+    if (adds.length > 0) {
+      diffs += Add(aDiffPos, adds)
+    }
+
+    diffs.result
+
+  }
+
+
+  def patch[T](src: Seq[T], diffs: Seq[Diff[T]]): Seq[T] = {
+
+    var builderLen = src.length
+    diffs foreach {
+      case Del(pos, len) => builderLen -= len
+      case Add(pos, data) => builderLen += data.length
+    }
+
+    val res = new ArrayBuffer[T](builderLen)
+    var srcPos = 0
+
+    diffs.sortBy(d => (d.pos, !d.isInstanceOf[Add[_]])) foreach { // deletions first
+      case Del(pos, len) =>
+        if (srcPos < pos) {
+          res ++= src.slice(srcPos, pos)
+        }
+        srcPos = pos + len
+      case Add(pos, values) =>
+        if (srcPos < pos) {
+          res ++= src.slice(srcPos, pos)
+        }
+        res ++= values
+        srcPos = pos
+    }
+
+    if (srcPos < src.length) {
+      res ++= src.slice(srcPos, src.length)
+    }
+
+    assert(res.length == builderLen)
+
+    res
+  }
+}
